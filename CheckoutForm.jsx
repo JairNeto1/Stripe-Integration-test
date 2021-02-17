3fa3a0c0579ad05d4be087e6498505ba62f3c7b8
Jair Neto <76796505+JairNeto1@users.noreply.github.com>
2021-02-17 19:51:10 -0300

    add files

---
+++ CheckoutForm.jsx
@@ -0,0 +1,108 @@
+import { useState } from "react";
+import styled from "@emotion/styled";
+import axios from "axios";
+
+import Row from "./prebuilt/Row";
+import BillingDetailsFields from "./prebuilt/BillingDetailsFields";
+import SubmitButton from "./prebuilt/SubmitButton";
+import CheckoutError from "./prebuilt/CheckoutError";
+
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+
+const CardElementContainer = styled.div`
+  height: 40px;
+  display: flex;
+  align-items: center;
+
+  & .StripeElement {
+    width: 100%;
+    padding: 15px;
+  }
+`;
+
+const CheckoutForm = ({ price, onSuccessfulCheckout }) => {
+  const [isProcessing, setProcessingTo] = useState(false);
+  const [checkoutError, setCheckoutError] = useState();
+
+  const stripe =useStripe();
+  const elements = useElements();
+
+  const handleFormSubmit = async ev => {
+    ev.preventDefault();
+
+    const billingDetails = {
+      name: ev.target.name.value,
+      email: ev.target.email.value,
+      address: {
+        city: ev.target.city.value,
+        line1: ev.target.address.value,
+        state: ev.target.state.value,
+        postal_code: ev.target.zip.value
+      }
+    };
+
+    setProcessingTo(true)
+
+    const { data: clientSecret } = await axios.post("/api/payment_intents", {
+      amount: price * 100
+    });
+    
+
+    const cardElement = elements.getElement(CardElement);
+
+   const paymentMethodReq = await stripe.createPaymentMethod ({
+     type: 'card' ,
+     card: cardElement ,
+      billing_details: billingDetails
+    });
+
+   const confirmedCarPayment = await stripe.confirmCardPayment(clientSecret, {
+     payment_method: paymentMethodReq.paymentMethod.id
+   });
+   
+    onSuccessfulCheckout ();
+  };
+ 
+  
+
+
+
+  const cardElementOpitionns = {
+
+    style: {
+      base: {
+        fontSize: "16px",
+        color: "#fff",
+        "::placeholder": {
+          color: "#87bbfd"
+        }
+      },
+      invalid: {
+        color:"#FFC7EE",
+        iconColor: "#FFC7EE"
+      }
+    },
+    hidePostalCode: true,
+  };
+
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <Row>
+        <BillingDetailsFields />
+      </Row>
+      <Row>
+        <CardElementContainer>
+          <CardElement options={cardElementOpitionns}/>
+        </CardElementContainer>
+      </Row>
+      {checkoutError && <CheckoutError>{checkoutError}</CheckoutError>}
+      <Row>
+        <SubmitButton disabled={isProcessing}>
+          {isProcessing ? "Processing..." : `Pay $${price}`}
+        </SubmitButton>
+      </Row>
+    </form>
+  );
+};
+
+export default CheckoutForm;
